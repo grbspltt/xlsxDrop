6e870360546f73a12d85db0a6ab3b112f8e06279
Kevin Splittgerber <kevin.splittgerber@gmail.com>
2016-05-19 10:35:18 -0700

    Logging, cache control, fix bug not calling next after mounting models to req.

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,34 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import logger from 'morgan'
 import models from './models/db'
 import routes from './routes'
 let app = express();
 
 export default models
 
+// Setup Logging
+// =============
+app.use(logger('tiny'));
 app.use(express.static('../../client/build'));
-app.use(bodyParser.json({limit:'50mb',extended:false, parameterLimit:100000}));
+app.use(bodyParser.json(/*{limit:'50mb',extended:false, parameterLimit:100000}*/));
 app.use(bodyParser.urlencoded({limit:'50mb',extended:false, parameterLimit:100000}));
 
-//setup cors
+// Setup CORS
+// ==========
 app.use(function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Cache-Control", "no-cache, no-store");
   next();
 });
 
-//load models on req.models
+// Load Models On req.models
+// =========================
 app.use((req,res,next)=>{
   req.models = models;
+  next();
 });
 app.use(routes);
 
