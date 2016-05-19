bfe7173bd1742ae45195033f6db3c691f7607854
Kevin Splittgerber <kevin.splittgerber@gmail.com>
2016-05-19 12:05:54 -0700

    Remove trailing slash

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,5 +2,5 @@
 // ====================================
 // =========== Constants ==============
 // ====================================
-export const SERVER_URL = 'http://localhost:8081/';
+export const SERVER_URL = 'http://localhost:8081';
 export const AUHSD_ACCOUNTING_URL = 'data/auhsdAccounting';
